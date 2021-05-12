2a633bab56ac104fec21fcd6382f314d04b5fd69
PuzzeOut <puzzleout@LAPTOP-H3PMF91C.localdomain>
2021-05-12 12:02:17 +0200

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
-def main():
+def primary():
    print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
+ # f = open("quotes.txt")
   #quotes = f.readlines()
   #f.close()
 
   #print(quotes)
 
-if __name__== "__main__":
-  main()
+if __name__== "primary":
+  primary()
